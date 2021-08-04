d89903ef60a37194ba1197a0bcbc8c4283f59df7
Sophie Chan <sophiechan103@gmail.com>
2021-08-04 04:31:02 +0000

    minor bug fix

--- public/js/mainpage.js
+++ public/js/mainpage.js
@@ -64,7 +64,7 @@ cancelInput.addEventListener("click", () => {
 
 //closes image display modal when the top-right "X" button is clicked
 closeImgView.addEventListener("click", () => {
-    imgDisplay.classList.remove("is-active");
+    closeModal();
 })
 
 //update file name in upload image modal when file is selected
